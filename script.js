efb71c72ef91f105f731d0065faeef5b3897188c
elzzup <guild0105@gmail.com>
2015-10-31 04:04:32 +0900

    Fix get response

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
-console.log($);
 $(function() {
-    $.post('https://raw.githubusercontent.com/twitter/hbc/master/hbc-twitter4j/src/test/resources/scrub-geo.json',
+    var API_URL = 'http://api.munisystem.net:3000/books';
+    $.get(API_URL,
         null,
         function(data) {
-            data = '[ { "id": 1, "name": "muni" }, { "id": 2, "name": "piyopiyo" }, { "id": 3, "name": "hogehoge" } ]';
-            data = JSON.parse(data);
             for (var i = 0; i < data.length; i++) {
                 $div = $('<div/>').append(
-                        $('<span/>').text('id: ' + data[i].id)
+                        $('<span/>').text('id: ' + data[i].user)
                 ).append(
                         $('<span/>').text('name: ' + data[i].name)
                 );
